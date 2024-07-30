ae66f7f97d2acbd868bb0fa1251fc1713764a22a
suzoo-dev <57764195+suzoo-dev@users.noreply.github.com>
2024-07-30 23:46:25 +0100

    added react query client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <> Hello </>;
+  return <QueryClientProvider client={queryClient}>Hello</QueryClientProvider>;
 }
 
 export default App;
